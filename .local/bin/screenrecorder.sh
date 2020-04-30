c0922bf7903ebcac585036a20e33399cb3889556
Niclas Larsson <niclas.larsson@jeppesen.com>
2020-04-30 20:00:50 +0200

    Notify before starting the screen recording.

--- .local/bin/screenrecorder.sh
+++ .local/bin/screenrecorder.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/zsh
 
-wf-recorder -f "$(xdg-user-dir VIDEOS)/$(date +'screenrecording_%Y-%m-%d-%H%M%S.mp4')"
-
 notify-send -t 5000 "Screen recorder started."
+wf-recorder -f "$(xdg-user-dir VIDEOS)/$(date +'screenrecording_%Y-%m-%d-%H%M%S.mp4')"
--- .local/bin/screenrecorder_selection.sh
+++ .local/bin/screenrecorder_selection.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/zsh
 
-wf-recorder -g "$(slurp)" -f "$(xdg-user-dir VIDEOS)/$(date +'screenrecording_%Y-%m-%d-%H%M%S.mp4')"
-
 notify-send -t 5000 "Screen recorder started."
+wf-recorder -g "$(slurp)" -f "$(xdg-user-dir VIDEOS)/$(date +'screenrecording_%Y-%m-%d-%H%M%S.mp4')"
