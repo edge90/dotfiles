ce4991ac4e5a928b3a08bb1e2c97ae4bcd086309
Niclas Larsson <niclas@edgesystems.se>
2020-11-11 22:07:12 +0100

    Some more testing.
    Restructure.
    Show signcolumn.
    language servers.

--- .config/nvim/lua/_lspconfig.lua
+++ .config/nvim/lua/_lspconfig.lua
@@ -1,21 +1,27 @@
 local lsp = require "nvim_lsp"
 
 local on_attach = function(client)
-  require "diagnostic".on_attach(client)
-  require "completion".on_attach(client)
+    require "diagnostic".on_attach(client)
+    require "completion".on_attach(client)
 end
 
 local servers = {
-  bashls = {},
-  ccls = {},
-  cssls = {},
-  html = {},
-  rls = {
-  },
-  vimls = {},
+    bashls = {},
+    ccls = {},
+    cssls = {},
+    dockerls = {},
+    html = {},
+    jsonls = {
+        cmd = { "json-languageserver", "--stdio" }
+    },
+    rls = {},
+    sumneko_lua = {
+        cmd = { "lua-language-server" }
+    },
+    vimls = {},
 }
 
 for server, config in pairs(servers) do
-  config.on_attach = on_attach
-  lsp[server].setup(config)
+    config.on_attach = on_attach
+    lsp[server].setup(config)
 end
--- .config/nvim/lua/_settings.lua
+++ .config/nvim/lua/_settings.lua
@@ -1,66 +1,64 @@
- 
-local wo = vim.wo
-local o = vim.o
+
+local api = vim.api
 local bo = vim.bo
 local g = vim.g
+local o = vim.o
+local wo = vim.wo
 
 
 -- main
-g.mapleader = " "
-o.clipboard = "unnamedplus"
-o.ve = "onemore"
-o.shortmess = "atI"
-g.python_host_prog = '/usr/bin/python2'
-g.python3_host_prog = '/usr/bin/python3.8'
-o.mouse = "a"
-o.backspace = "indent,start"
 g.autochdir = true
+g.mapleader = " "
+g.python3_host_prog = '/usr/bin/python3.8'
+g.python_host_prog = '/usr/bin/python2'
+o.backspace = "indent,start"
+o.clipboard = "unnamedplus"
+o.mouse = "a"
+o.shortmess = "atI"
+o.ve = "onemore"
 
 -- interface
-vim.api.nvim_command("colorscheme gruvbox")
-wo.cursorline = true
-wo.number = true
-wo.relativenumber = true
-wo.cursorcolumn = false
+api.nvim_command("colorscheme gruvbox")
+g.listchars = [[tab:|]]
+o.fillchars = [[vert: ]]
+o.hidden = true
+o.laststatus = 0
+o.list = true
+o.ruler = false
+o.showmode = false
 o.termguicolors = false
 o.title = true
 o.titlestring = "%t"
-o.ruler = false
-o.list = true
---vim.api.nvim_command("set list")
---vim.api.nvim_command([[set listchars=tab:!·,trail:·]])
--- o.lazyredraw = true
-o.list = true
-g.listchars = [[tab:|]]
-o.fillchars = [[vert: ]]
 o.updatetime = 50
-o.hidden = true
+wo.cursorcolumn = false
+wo.cursorline = true
+wo.number = true
+wo.relativenumber = true
+wo.signcolumn = "yes"
 wo.wrap = false
-o.showmode = false
-o.laststatus = 0
 
 -- tabs
+bo.expandtab = true
+bo.shiftwidth = 4
+bo.smartindent = true
+bo.tabstop = 4
+o.expandtab = true
+o.shiftwidth = 4
 o.smartindent = true
 o.tabstop = 4
-o.shiftwidth = 4
-o.expandtab = true
-bo.expandtab = true
-bo.tabstop = 4
-bo.smartindent = true
-bo.shiftwidth = 4
 
 -- case check
-o.ignorecase = true
-o.smartcase = true
 o.hlsearch = true
-o.incsearch = true
 o.ignorecase = true
+o.ignorecase = true
+o.incsearch = true
+o.smartcase = true
 o.smartcase = true
 
 -- backup, undo, swap options
-o.undofile = false
 o.backup = false
-o.writebackup = false
 o.swapfile = false
+o.undofile = false
+o.writebackup = false
 
 vim.api.nvim_set_keymap('n', '<leader>vs', ':LazyGit<CR>', { noremap = true, silent = true })
